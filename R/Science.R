0bf357d1d0d01c1552c379718322098f020ed65c
Phil Chalmers <rphilip.chalmers@gmail.com>
2012-02-13 22:19:23 -0500

    Moved data descriptions to .R files

---
+++ R/Science.R
@@ -0,0 +1,11 @@
+#' Description of Science data
+#' 
+#' A 4 item data set borrowed from \code{\link[ltm]{ltm}} package, first example 
+#' of the \code{\link[ltm]{grm}} function. See more complete documentation therein.  
+#' 
+#' 
+#' @name Science
+#' @docType data
+#' @author Phil Chalmers \email{rphilip.chalmers@@gmail.com}
+#' @keywords data
+NULL
