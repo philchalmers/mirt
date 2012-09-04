bb6437dc4a8c06970b2b2dd8aad87eae85d4a0a8
philchalmers <rphilip.chalmers@gmail.com>
2012-09-04 14:28:42 -0400

    Dummy function for MG via EM

---
+++ R/EMstep.group.R
@@ -0,0 +1,10 @@
+EM.group <- function(pars, constrain, PrepList, list, debug)
+{
+    if(debug == 'EM.group') browser()
+    
+    
+#     ret <- list(pars=pars, cycles = cycles - BURNIN - SEMCYCLES, info=as.matrix(info), 
+#                 longpars=longpars, converge=converge)
+#     ret
+}
+
