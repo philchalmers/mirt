fe637b560cc4dac50f8c352a51332e248cee3c1f
philchalmers <rphilip.chalmers@gmail.com>
2013-01-15 10:23:26 -0500

    Update testinfo to work with MG objects

--- R/testinfo.R
+++ R/testinfo.R
@@ -7,6 +7,8 @@
 #' @param Theta a matrix of latent trait values
 #' @param degrees a vector of angles in degrees that are between 0 and 90 that jointly sum to 90. 
 #' Only applicable when the input object is multidimensional
+#' @param group a number signifying which group the item should be extracted from (applies to 
+#' 'MultipleGroupClass' objects only)
 #' @keywords information
 #' @export testinfo
 #' @examples 
@@ -20,14 +22,13 @@
 #' plot(Theta, tinfo, type = 'l')
 #' 
 #' } 
-testinfo <- function(x, Theta, degrees = NULL){    
+testinfo <- function(x, Theta, degrees = NULL, group = NULL){
     if(is(x, 'MultipleGroupClass')) 
-        stop('Multiple group objects not supported. Create a 
-             submodel using mirt() with fixed and non-estimated parameters.')
-    J <- length(x@pars) - 1
+        J <- length(x@cmods[[1]]@pars) - 1
+    else J <- length(x@pars) - 1
     info <- 0
     for(i in 1:J){
-        item <- extract.item(x, i)
+        item <- extract.item(x, i, group=group)
         info <- info + iteminfo(item, Theta=Theta, degrees=degrees)
     }
     return(info)
