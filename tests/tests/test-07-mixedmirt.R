e7d02c391f434e714d63839e046209aad2342135
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-05-08 20:38:52 -0400

    fix tests

--- tests/tests/test-07-mixedmirt.R
+++ tests/tests/test-07-mixedmirt.R
@@ -17,29 +17,29 @@ test_that('mixed dich', {
     mod0 <- mirt(data, 1, 'Rasch', covdata=covdata, formula = ~ group + pseudoIQ, verbose=FALSE)
     expect_equal(mod0@Fit$logLik, -4058.968, tolerance = 1e-2)
     cfs <- coef(mod0, digits = 10)
-    expect_equal(as.numeric(cfs$lr.betas), c(0.0000000, 0.9548921, 1.9383165, 0.1877870), tolerance=1e-4)
+    expect_equal(as.numeric(cfs$lr.betas), c(0.0000000, 0.8916977, 1.9757340, 0.2168971), tolerance=1e-4)
     set.seed(1234)
     plaus <- fscores(mod0, plausible.draws = 2)
-    expect_equal(plaus[[1]][1:4], c(-0.6070500, -0.2729041, -0.3763773, -0.4946551),
+    expect_equal(plaus[[1]][1:4], c(-0.6875763, -0.3252222, -0.3528101, -0.4759003),
                  tolerance = 1e-4)
     require(boot, quietly=TRUE, warn.conflicts=FALSE)
     set.seed(1)
     bs <- boot.mirt(mod0, R = 3)
     expect_is(bs, 'boot')
     fs <- fscores(mod0, full.scores.SE=TRUE, full.scores=FALSE)
-    expect_equal(as.numeric(head(fs)), c(-0.2820982,-0.5666356,-0.4924289,-0.3397344,-0.4862802,-0.4766593,0.2692081,0.2708598,0.2704379,0.2695491,0.2704026,0.2703473), tolerance=1e-4)
+    expect_equal(as.numeric(head(fs)), c(-0.3326595,-0.6466125,-0.479462,-0.3070204,-0.4769287,-0.5488554,0.2940327,0.2963648,0.2951399,0.2938372,0.295121,0.2956538), tolerance=1e-4)
 
     #group as a fixed effect predictor (aka, uniform dif)
     mod1 <- mixedmirt(data, covdata, model, fixed = ~ 0 + items + group,
                                        verbose = FALSE, draws = 1)
     expect_is(mod1, 'MixedClass')
     cfs <- as.numeric(do.call(c, coef(mod1, digits=4)))
-    expect_equal(cfs, c(1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-1.6979,-1.8905,-1.5053,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-2.1101,-2.3119,-1.9083,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-1.7048,-1.8975,-1.512,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-1.0493,-1.2334,-0.8652,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-0.3151,-0.4994,-0.1309,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-1.2453,-1.4313,-1.0594,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-1.6843,-1.8766,-1.4919,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-2.125,-2.3272,-1.9229,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,-2.029,-2.2287,-1.8292,0,NA,NA,1,NA,NA,1.111,0.9628,1.2593,2.2422,2.0891,2.3954,1,NA,NA,1.6114,1.3404,1.8824,0,NA,NA,1,NA,NA,0,NA,NA,0.1035,0.0541,0.1529),
+    expect_equal(cfs, c(1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-1.7432,-1.9407,-1.5458,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-2.0608,-2.2651,-1.8565,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-1.7014,-1.898,-1.5047,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-1.0408,-1.2289,-0.8527,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-0.2658,-0.4541,-0.0775,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-1.3741,-1.5656,-1.1825,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-1.6805,-1.8768,-1.4842,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-2.121,-2.3268,-1.9152,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,-2.0015,-2.2043,-1.7986,0,NA,NA,1,NA,NA,1.075,0.9182,1.2318,2.3263,2.1645,2.488,1,NA,NA,1.5431,1.2778,1.8084,0,NA,NA,1,NA,NA,0,NA,NA,0.1381,0.1015,0.1747),
                  tolerance = 1e-2)
     names <- wald(mod1)
     L <- matrix(c(1, numeric(length(names) - 1L)), 1L)
     wld <- wald(mod1, L, C=as.numeric(L))
-    expect_equal(wld$W[1], 2.156415, tolerance = 1e-4)
+    expect_equal(wld$W[1], .8784299, tolerance = 1e-4)
     set.seed(1)
     bs <- boot.mirt(mod1, R=2)
     expect_is(bs, 'boot')
@@ -47,18 +47,18 @@ test_that('mixed dich', {
     mod1a <- mixedmirt(data, covdata, model, fixed = ~ 0 + items + group, SE=FALSE,
                       verbose = FALSE, draws = 1, internal_constraints = FALSE)
     cfs <- as.numeric(do.call(c, coef(mod1a, digits=4)))
-    expect_equal(cfs, c(0.7882,1.6403,1,-1.3455,0,1,1.3836,2.731,1,-2.4362,0,1,2.4475,4.3307,1,-3.1301,0,1,0.3616,0.6616,1,-0.2824,0,1,1.5031,2.649,1,-0.5388,0,1,1.4038,3.1316,1,-1.6373,0,1,0.9761,1.8945,1,-1.4983,0,1,1.4147,2.563,1,-2.3848,0,1,0.8402,1.9187,1,-1.7901,0,1,0.9049,2.062,1,1.6933,0,1,0,0.1088),
+    expect_equal(cfs, c(0.8023,1.5944,1,-1.3592,0,1,1.3988,2.9293,1,-2.4552,0,1,2.4305,4.4838,1,-3.1509,0,1,0.307,0.7794,1,-0.2878,0,1,1.4845,3.0533,1,-0.5464,0,1,1.2012,3.0349,1,-1.6528,0,1,0.9915,1.9697,1,-1.513,0,1,1.4304,2.6556,1,-2.4036,0,1,0.8535,2.0584,1,-1.8064,0,1,0.8374,2.3511,1,1.6148,0,1,0,0.1489),
                  tolerance = 1e-2)
 
     mod_items <- mixedmirt(data, covdata, model, fixed = ~ 1, SE=FALSE, random = ~ 1|items,
                        verbose = FALSE, draws = 1)
     cfs <- c(coef(mod_items)[['GroupPars']], coef(mod_items)[['items']])
-    expect_equal(cfs[1:3], c(0, 1.005, 1.143), tolerance = 1e-3)
+    expect_equal(cfs[1:3], c(0,1.128,1.076), tolerance = 1e-3)
 
     mod_items.group <- mixedmirt(data, covdata, model, fixed = ~ 1, SE=FALSE, random = ~ 1|items:group,
                            verbose = FALSE, draws = 1)
     cfs <- c(coef(mod_items.group)[['GroupPars']], coef(mod_items.group)[['items:group']])
-    expect_equal(cfs[1:3], c(0, .115, 2.254), tolerance = 1e-3)
+    expect_equal(cfs[1:3], c(0, .137, 2.334), tolerance = 1e-3)
     set.seed(1)
     bs <- boot.mirt(mod_items.group, R=2)
     expect_is(bs, 'boot')
@@ -70,7 +70,7 @@ test_that('mixed dich', {
     expect_is(mod1b, 'MixedClass')
     expect_equal(extract.mirt(mod1b, 'df'), 1001)
     cfs <- as.numeric(do.call(c, coef(mod1b, digits=4)))
-    expect_equal(cfs, c(1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,-6e-04,-0.2538,0.2526,-1.7908,-1.9861,-1.5955,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,0.133,-0.1237,0.3896,-2.2139,-2.4201,-2.0077,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,0.056,-0.1629,0.2749,-1.7997,-1.9955,-1.6039,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,-3.132,-4.2471,-2.0169,-1.05,-1.4039,-0.6962,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,0.0428,-0.1931,0.2787,-0.3885,-0.5725,-0.2045,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,0.0864,-0.1338,0.3067,-1.3339,-1.5223,-1.1454,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,-0.0749,-0.3338,0.1841,-1.7758,-1.9707,-1.5809,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,0.2577,1e-04,0.5153,-2.2396,-2.4487,-2.0305,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,-0.1304,-0.3719,0.1111,-2.1229,-2.3255,-1.9204,0,NA,NA,1,NA,NA,1.2111,1.0631,1.3591,2.4128,2.2574,2.5681,-0.1141,-0.5071,0.279,1.5521,1.2816,1.8226,0,NA,NA,1,NA,NA,0,NA,NA,1,NA,NA),
+    expect_equal(cfs, c(1.0652,NaN,NaN,2.3363,NaN,NaN,0.6912,NaN,NaN,-1.7727,NaN,NaN,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,0.3748,NaN,NaN,-2.064,-2.2495,-1.8785,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,0.1637,NaN,NaN,-1.6932,NaN,NaN,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,-0.0519,NaN,NaN,-1.0393,NaN,NaN,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,0.3548,NaN,NaN,-0.2618,NaN,NaN,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,0.0425,NaN,NaN,-1.3679,NaN,NaN,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,0.3055,-0.113,0.724,-1.6781,-1.7586,-1.5976,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,0.1485,NaN,NaN,-2.1073,-2.1676,-2.047,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,1.0666,NaN,NaN,-2.1148,NaN,NaN,0,NA,NA,1,NA,NA,1.0652,NaN,NaN,2.3363,NaN,NaN,0.831,NaN,NaN,1.7523,1.4181,2.0865,0,NA,NA,1,NA,NA,0,NA,NA,1,NA,NA),
                  tolerance = 1e-2)
 
     covdata$group <- factor(rep(paste0('G',1:50), each = N/50))
@@ -79,7 +79,7 @@ test_that('mixed dich', {
     expect_is(rmod1, 'MixedClass')
     expect_equal(extract.mirt(rmod1, 'df'), 1011)
     cfs <- as.numeric(do.call(c, coef(rmod1, digits=4)))
-    expect_equal(cfs[124:129], c(0.0536, 0.0397, 0.0676, 1.0902, 0.6417, 1.5387), tolerance = 1e-2)
+    expect_equal(cfs[124:129], c(0.0733, 0.0469, 0.0998, 1.1051, 0.6519, 1.5583), tolerance = 1e-2)
 
     #polytomous
     covdat <- data.frame(group = rep(c('m', 'f'), nrow(Science)/2))
@@ -132,17 +132,17 @@ test_that('mixed dich', {
     mod <- mixedmirt(dat, covdata = covdata, 1, itemtype = '2PL', fixed = ~ 0 + items,
                      lr.fixed = ~ group, verbose=FALSE, SE=TRUE)
     cfs <- coef(mod, digits=10, printSE=TRUE)
-    expect_equal(as.numeric(cfs$lr.betas)[-c(1:2)], c(1.4712503, 0.2312698, 2.9707061, 0.2363475),
+    expect_equal(as.numeric(cfs$lr.betas)[-c(1:2)], c(1.4192996, 0.1366249, 3.1089497, 0.1664278),
                  tolerance=1e-4)
-    expect_equal(extract.mirt(mod, 'logLik'), -4360.41, tolerance = 1e-2)
+    expect_equal(extract.mirt(mod, 'logLik'), -4314.043, tolerance = 1e-2)
     expect_equal(extract.mirt(mod, 'df'), 1001)
 
     mod2 <- mixedmirt(dat, covdata = covdata, 1, itemtype = 'Rasch', fixed = ~ 0 + items,
                      lr.fixed = ~ group, verbose=FALSE, draws=1, SE=FALSE)
     so <- summary(mod2, verbose=FALSE)
     expect_equal(as.numeric(c(so$random$Theta, so$lr.out[,1])),
-                 c(0.1961649, 0.0000000, 0.7167553, 1.5104188), tolerance=1e-4)
-    expect_equal(extract.mirt(mod2, 'logLik'), -4688.784, tolerance = 1e-4)
+                 c(0.1978728, 0.0000000, 0.7181347, 1.7006670), tolerance=1e-4)
+    expect_equal(extract.mirt(mod2, 'logLik'), -4649.466, tolerance = 1e-4)
     set.seed(1)
     bs <- boot.mirt(mod2, R = 3)
     expect_is(bs, 'boot')
@@ -153,11 +153,11 @@ test_that('mixed dich', {
     mod <- mixedmirt(dat, covdata = covdata, 1, fixed = ~ 0 + items, SE=FALSE,
                      random = ~ -1 + cut|group, verbose=FALSE, draws=1)
     so <- summary(mod, verbose=FALSE)
-    expect_equal(as.numeric(diag(so$random$group)), c(0.6211201, 0.6262019), tolerance = 1e-4)
+    expect_equal(as.numeric(diag(so$random$group)), c(0.6230722, 0.3828860), tolerance = 1e-4)
 
     mod <- mixedmirt(dat, covdata = covdata, 1, fixed = ~ 0 + items, SE=FALSE,
                      random = ~ -1 + theta|group, verbose=FALSE, draws=1)
     so <- summary(mod, verbose=FALSE)
-    expect_equal(as.numeric(diag(so$random$group)), c(0.05447179, 0.51292915), tolerance = 1e-4)
+    expect_equal(as.numeric(diag(so$random$group)), c(0.2089722, 0.3948205), tolerance = 1e-4)
 
 })
--- tests/tests/test-10-extras.R
+++ tests/tests/test-10-extras.R
@@ -34,37 +34,36 @@ test_that('extras', {
     modideal <- mirt(dataset1, model = mirt.model('F1 = 1-6
                                                   F2 = 5-10'), 'ideal', verbose = FALSE)
     cfs <- as.numeric(coef(modideal, digits=5, verbose=FALSE)[[5]])
-    expect_equal(extract.mirt(modideal, 'logLik'), -6435.647, tolerance = 1e-3)
-    expect_equal(cfs, c(0.73123, 1.26069, -1.41785), tolerance = 1e-3)
+    expect_equal(extract.mirt(modideal, 'logLik'), -6408.54, tolerance = 1e-3)
+    expect_equal(cfs, c(0.30446, 0.40742, -1.43222), tolerance = 1e-3)
 
     acov <- fscores(mod1, return.acov=TRUE, full.scores=FALSE)
     expect_equal(acov[[1]][1], 0.4799239, tolerance=1e-3)
     acov <- fscores(mod1, return.acov=TRUE, full.scores=TRUE)
     expect_equal(acov[[500]][1], 0.4987313, tolerance=1e-3)
     acov <- fscores(model1a, return.acov=TRUE, full.scores=TRUE)
-    expect_equal(acov[[500]][1], .3452783, tolerance=1e-3)
+    expect_equal(acov[[500]][1], .325383, tolerance=1e-3)
     boot <- boot.mirt(model1a, R=5)
     cfs <- boot$t0
     cfs <- cfs[cfs != 0 & cfs != 1]
-    expect_equal(as.numeric(cfs), c(1.03027, -0.03172, 1.18018, 1.36523, 1.1824, 0.10923, 0.94459, 0.36701, 1.09781, -0.53252, 0.38686, 0.48484, 1.4409, -0.6983, 0.80128, -0.22106, 0.80456, 0.83075, 0.7102, 0.24493, 0.67924, 0.10731, 0.47428, 1.27867, 1.42737, 0.41311, 1.29134, 0.51946, 1.36572, -0.39269, 0.64571, 0.60175, 1.44829, -0.4938, 1.2331, -0.20888, 1.08338, 0.95549, 0.98608, 0.1816),
+    expect_equal(as.numeric(cfs), c(1.200932,0.03966039,1.202983,1.253283,1.162713,0.1054175,0.935932,0.4109794,1.202011,-0.5220202,0.5644482,0.6966761,1.219665,-0.7151297,0.9018733,-0.1465739,0.855102,0.7757409,0.7616776,0.1306053,0.3678973,-0.1117068,0.4865936,1.258095,1.270467,0.3109331,1.126136,0.3280035,1.441583,-0.3823625,0.5785964,0.6331239,1.531089,-0.5576562,1.289808,-0.1500941,1.141839,0.94723,0.9748633,0.2646415),
                  tolerance=1e-3)
-    # PLCI <- PLCI.mirt(mirt(Science, 1, verbose=FALSE), parnum=c(1,2))
-    # expect_equal(c(PLCI$lower_2.5, PLCI$upper_97.5), c(0.7008617, 4.0112247, 1.4530232, 5.9667792),
-    #              tolerance=1e-3)
-    DIFF <- suppressMessages(DIF(model1a, which.par='d', items2test = 1:3))
+    PLCI <- PLCI.mirt(mirt(Science, 1, verbose=FALSE), parnum=c(1,2))
+    expect_equal(c(PLCI$lower_2.5, PLCI$upper_97.5), c(0.7008617, 4.0112247, 1.4530232, 5.9667792),
+                  tolerance=1e-3)
+    DIFF <- suppressMessages(DIF(model1a, which.par='d', items2test = c(1,3)))
     expect_is(DIFF, 'list')
-    expect_equal(DIFF[[1L]][2,'logLik'], -12508.15, tolerance = 1e-3)
-    DIFF2 <- suppressMessages(DIF(model2, which.par=c('a1', 'd'), items2test = 1:3, scheme='drop'))
+    expect_equal(DIFF[[1L]][2,'logLik'], -12519.45, tolerance = 1e-3)
+    DIFF2 <- suppressMessages(DIF(model2, which.par=c('a1', 'd'), items2test = c(1,3), scheme='drop'))
     expect_is(DIFF2, 'list')
-    expect_equal(DIFF2[[1L]][,'logLik'], c(-12534.13, -12528.83), tolerance = 1e-3)
+    expect_equal(DIFF2[[1L]][,'logLik'], c(-12562.00, -12539.75), tolerance = 1e-3)
 
     WALD <- suppressMessages(DIF(model1a, which.par='d', items2test = 1:3, Wald=TRUE))
     expect_is(WALD, 'list')
-    expect_equal(WALD[[1]]$W[1], 1.779187, tolerance = 1e-3)
-    expect_equal(WALD[[1]]$p[1], .1822492, tolerance = 1e-3)
+    expect_equal(WALD[[1]]$W[1], 2.101652, tolerance = 1e-3)
+    expect_equal(WALD[[1]]$p[1], .1471401, tolerance = 1e-3)
     WALD2 <- suppressMessages(DIF(model1a, which.par=c('a1', 'd'), Wald=TRUE, p.adjust = 'fdr'))
-    expect_equal(as.numeric(WALD2$adj_pvals), c(0.0777, 0.0010, 0.0777, 0.1611, 0.1652, 0.1611,
-                                                0.2662, 0.0777, 0.2344, 0.1652), tolerance = 1e-3)
+    expect_equal(as.numeric(WALD2$adj_pvals), c(2.027518e-06,0.0001081836,0.2437397,0.370737,0.2437397,0.8028216,0.2010698,0.2010698,0.2437397,0.2437397), tolerance = 1e-3)
     extr.2 <- extract.item(mod1, 2)
     Theta <- matrix(seq(-6,6, length.out=200))
     expected <- expected.item(extr.2, Theta)
@@ -97,7 +96,7 @@ test_that('extras', {
     pick <- c('a1', 'a2', 'd1', 'd2', 'd3')
     expect_true(sum(abs(s1[,pick] - s2[,pick])) < 1e-10)
     mod3 <- mirt(dat, 2, 'gpcm', gpcm_mats = mats, TOL = 1e-2, verbose=FALSE)
-    expect_equal(extract.mirt(mod3, 'logLik'), -3708.0, tolerance = 1e-4)
+    expect_equal(extract.mirt(mod3, 'logLik'), -3708.216, tolerance = 1e-4)
     cfs <- as.vector(coef(mod3, simplify=TRUE, digits = 5)$items)
     expect_equal(cfs, c(-1.23177,-2.76946,-1.54897,-1.34947,-0.41973,-0.45958,-0.71595,-0.58334,-0.29564,4.58694,0.36752,-0.31281,0.4241,-2.90723,-0.16516,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,0,0,0,0,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,3.69493,7.87579,3.59178,3.00087,2.60806,2.7068,2.83078,2.07429,6.50601,10.34317,5.36289,4.0315,5.42778,5.57857,4.69492,3.43515,4.9848,4.17678,3.89073,2.40695,5.74,5.87555,5.00542,3.86719,NA,NA,NA,NA,4,4,4,4,NA,NA,NA,NA,5,5,5,5,NA,NA,NA,NA,0,0,0,0,NA,NA,NA,NA,0,0,0,0,NA,NA,NA,NA,5.43095,5.52149,4.54781,3.38757,NA,NA,NA,NA,3.98583,3.27075,2.90466,1.19029), tolerance=1e-4)
 
