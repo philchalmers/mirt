c89719fea91d3f8f1dbdb942d013ba177db654e5
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-06-12 20:13:57 -0400

    add functional inputs

--- R/PLCI.mirt.R
+++ R/PLCI.mirt.R
@@ -10,6 +10,16 @@
 #' @param parnum a numeric vector indicating which parameters to estimate.
 #'   Use \code{\link{mod2values}} to determine parameter numbers. If \code{NULL}, all possible
 #'   parameters are used
+#' @param fun a function used to define a transformation of 1 or more parameters of the
+#'   form \code{fun(par)}, where \code{par} is one or more parameters indicated by
+#'   the \code{parnum} input. This function must return a single numeric value
+#' @param inv.fun the inverse transformation function of \code{fun} of the form
+#'   \code{inv.fun(newpar, par)}, where \code{par} is defined as before, however
+#'   \code{newpar} is the the transformed parameter(s) (e.g., returned from \code{fun(par)}).
+#'   Note that the length of the returned vector should be the same as the original length
+#'   of \code{par}
+#' @param fun.lower lower bound used when \code{fun} is supplied. Default is -Inf
+#' @param fun.upper upper bound used when \code{fun} is supplied. Default is Inf
 #' @param ... additional arguments to pass to the estimation functions
 #'
 #' @author Phil Chalmers \email{rphilip.chalmers@@gmail.com}
@@ -38,8 +48,19 @@
 #' result3 <- PLCI.mirt(mod2, parnum=parnum)
 #' result3
 #'
+#' #####
+#' # transformation example for computing PL CI of difficulty parameter
+#' dat <- expand.table(LSAT7)
+#' mod <- mirt(dat, 1)
+#'
+#' fun <- function(par) -par[2]/par[1]
+#' inv.fun <- function(newpar, par) c(par[1], -newpar * par[1])
+#'
+#' PLCI.mirt(mod, parnum=c(1,2), fun=fun, inv.fun=inv.fun)
+#'
 #' }
-PLCI.mirt <- function(mod, alpha = .05, parnum = NULL, ...){
+PLCI.mirt <- function(mod, alpha = .05, parnum = NULL, fun = NULL, inv.fun = NULL,
+                      fun.lower = -Inf, fun.upper = Inf, ...){
 
     #silently accepts print_debug = TRUE for printing the minimization criteria
 
@@ -57,23 +78,28 @@ PLCI.mirt <- function(mod, alpha = .05, parnum = NULL, ...){
     }
 
     f.min <- function(value, dat, model, which, sv, get.LL, large, parprior, parnames, asigns,
-                      PrepList, print_debug = FALSE, ...){
-        sv$est[which] <- FALSE
-        sv$value[which] <- value
-        if(sv$class[which] == 'graded'){
-            if(!(sv$name[which] %in% paste0('a', 1L:30L))){
-                itemname <- sv$item[which]
-                itemnum <- sv$parnum[which]
-                itemsv <- sv[sv$item == itemname & !(sv$name %in% paste0('a', 1L:30L)), ]
-                ds <- dsnew <- itemsv$value
-                srtds <- sort(ds, decreasing = TRUE)
-                if(!all(ds == srtds)){
-                    est <- itemsv$est
-                    if(est[1]) dsnew[1L] <- max(ds) + 1
-                    if(est[length(est)]) dsnew[length(est)] <- min(ds) - 1
-                    seqd <- seq(from=dsnew[1L], to=dsnew[length(est)], length.out = length(ds))
-                    ds[est] <- seqd[est]
-                    sv$value[itemsv$parnum] <- ds
+                      PrepList, fun, inv.fun, print_debug = FALSE, ...){
+        if(!is.null(fun)){
+            sv$est[parnums] <- FALSE
+            sv$value[parnums] <- inv.fun(value, sv$value[parnums])
+        } else {
+            sv$est[which] <- FALSE
+            sv$value[which] <- value
+            if(sv$class[which] == 'graded'){
+                if(!(sv$name[which] %in% paste0('a', 1L:30L))){
+                    itemname <- sv$item[which]
+                    itemnum <- sv$parnum[which]
+                    itemsv <- sv[sv$item == itemname & !(sv$name %in% paste0('a', 1L:30L)), ]
+                    ds <- dsnew <- itemsv$value
+                    srtds <- sort(ds, decreasing = TRUE)
+                    if(!all(ds == srtds)){
+                        est <- itemsv$est
+                        if(est[1]) dsnew[1L] <- max(ds) + 1
+                        if(est[length(est)]) dsnew[length(est)] <- min(ds) - 1
+                        seqd <- seq(from=dsnew[1L], to=dsnew[length(est)], length.out = length(ds))
+                        ds[est] <- seqd[est]
+                        sv$value[itemsv$parnum] <- ds
+                    }
                 }
             }
         }
@@ -91,22 +117,31 @@ PLCI.mirt <- function(mod, alpha = .05, parnum = NULL, ...){
     }
 
     LLpar <- function(parnum, parnums, parnames, lbound, ubound, dat, model, large,
-                      sv, get.LL, parprior, asigns, PrepList, pars, maxLL, ...){
+                      sv, get.LL, parprior, asigns, PrepList, pars, maxLL,
+                      fun, inv.fun, ...){
         TOL <- .001
-        lower <- ifelse(lbound[parnum] == -Inf, -15, lbound[parnum])
-        upper <- ifelse(ubound[parnum] == Inf, 15, ubound[parnum])
-        mid <- pars[parnum]
-        if(parnames[parnum] %in% c('g', 'u')){
-            lower <- 0
-            upper <- 1
-        } else if(parnames[parnum] %in% paste0('COV_', 1:30, 1:30)){
-            lower <- 1e-4
+        if(is.null(fun)){
+            lower <- ifelse(lbound[parnum] == -Inf, -15, lbound[parnum])
+            upper <- ifelse(ubound[parnum] == Inf, 15, ubound[parnum])
+            mid <- pars[parnum]
+            if(parnames[parnum] %in% c('g', 'u')){
+                lower <- 0
+                upper <- 1
+            } else if(parnames[parnum] %in% paste0('COV_', 1:30, 1:30)){
+                lower <- 1e-4
+            }
+        } else {
+            lower <- ifelse(lbound == -Inf, -30, lbound)
+            upper <- ifelse(ubound == Inf, 30, ubound)
+            mid <- fun(pars[parnums])
         }
+
         if(mid > lower){
             opt.lower <- try(uniroot(f.min, c(lower, mid), dat=dat, model=model,
                                  large=large, which=parnums[parnum], sv=sv, get.LL=get.LL,
                                  parprior=parprior, parnames=parnames, asigns=asigns,
-                                 PrepList=PrepList, ..., f.upper=maxLL-get.LL, tol = TOL/10),
+                                 PrepList=PrepList, fun=fun, inv.fun=inv.fun,
+                                 ..., f.upper=maxLL-get.LL, tol = TOL/10),
                              silent = TRUE)
             if(is(opt.lower, 'try-error')) opt.lower <- list(root = lower, f.root=1e10)
         } else opt.lower <- list(root = lower, f.root=1e10)
@@ -114,7 +149,8 @@ PLCI.mirt <- function(mod, alpha = .05, parnum = NULL, ...){
             opt.upper <- try(uniroot(f.min, c(mid, upper), dat=dat, model=model,
                                  large=large, which=parnums[parnum], sv=sv, get.LL=get.LL,
                                  parprior=parprior, parnames=parnames, asigns=asigns,
-                                 PrepList=PrepList, ..., f.lower=maxLL-get.LL, tol = TOL/10),
+                                 PrepList=PrepList, fun=fun, inv.fun=inv.fun,
+                                 ..., f.lower=maxLL-get.LL, tol = TOL/10),
                              silent = TRUE)
             if(is(opt.upper, 'try-error')) opt.upper <- list(root = upper, f.root=1e10)
         } else opt.upper <- list(root = upper, f.root=1e10)
@@ -140,34 +176,46 @@ PLCI.mirt <- function(mod, alpha = .05, parnum = NULL, ...){
     large <- mirt(mod@Data$data, mod@Model$model, large = TRUE)
     as <- matrix(sv$value[sv$name %in% paste0('a', 1L:30L)], ncol(dat))
     asigns <- sign(as)
+    if(!is.null(fun) && is.null(parnum))
+        stop('When supplying a tranformation function you must supply parnum')
     if(!is.null(parnum)){
         tmp <- sv$parnum %in% parnum
         pars <- sv$value[tmp]
-        LL.upper.crit <- LL.lower.crit <- pars
         parnums <- sv$parnum[tmp]
         itemtypes <- sv$class[tmp]
-        parnames <- sv$name[tmp]
-        lbound <- sv$lbound[tmp]
-        ubound <- sv$ubound[tmp]
+        if(!is.null(fun)){
+            lbound <- fun.lower
+            ubound <- fun.upper
+            which_parnums <- 1L
+        } else {
+            parnames <- sv$name[tmp]
+            lbound <- sv$lbound[tmp]
+            ubound <- sv$ubound[tmp]
+            which_parnums <- 1L:length(parnums)
+        }
     } else {
         pars <- sv$value
-        pars <- LL.upper.crit <- LL.lower.crit <- pars[sv$est]
         parnums <- sv$parnum[sv$est]
         itemtypes <- sv$class[sv$est]
         parnames <- sv$name[sv$est]
         lbound <- sv$lbound[sv$est]
         ubound <- sv$ubound[sv$est]
+        which_parnums <- 1L:length(parnums)
     }
     LL <- mod@Fit$logLik
     get.LL <- LL - qchisq(1-alpha, 1)/2
-    result <- mySapply(X=1L:length(parnums), FUN=LLpar, pars=pars, parnums=parnums, asigns=asigns,
+    result <- mySapply(X=which_parnums, FUN=LLpar, pars=pars, parnums=parnums, asigns=asigns,
                        parnames=parnames, lbound=lbound, ubound=ubound, dat=dat,
                        model=model, large=large, sv=sv, get.LL=get.LL, parprior=parprior,
-                       PrepList=PrepList, maxLL=LL, ...)
+                       PrepList=PrepList, maxLL=LL, fun=fun, inv.fun=inv.fun, ...)
     colnames(result) <- c(paste0('lower_', alpha/2*100), paste0('upper_', (1-alpha/2)*100),
                           'lower_conv', 'upper_conv')
-    ret <- data.frame(Item=sv$item[parnums], class=itemtypes, parnam=sv$name[parnums],
-                      parnum=parnums, value=pars, result, row.names=NULL)
+    ret <- if(!is.null(fun)){
+        data.frame(parnam='fun(par)', value=fun(pars), result, row.names=NULL)
+    } else {
+        data.frame(Item=sv$item[parnums], class=itemtypes, parnam=sv$name[parnums],
+                   parnum=parnums, value=pars, result, row.names=NULL)
+    }
     ret$lower_conv <- as.logical(ret$lower_conv)
     ret$upper_conv <- as.logical(ret$upper_conv)
     ret
