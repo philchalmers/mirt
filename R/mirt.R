3f9ba5fc129f0b20d767b62ed2d32b537e5d761d
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-10-04 09:56:21 -0400

    add point about data.matrix

--- R/mirt.R
+++ R/mirt.R
@@ -239,7 +239,8 @@
 #'
 #' @aliases mirt
 #' @param data a \code{matrix} or \code{data.frame} that consists of
-#'   numerically ordered data, with missing data coded as \code{NA}
+#'   numerically ordered data, with missing data coded as \code{NA} (to convert from an ordered factor
+#'   \code{data.frame} see \code{\link{data.matrix}})
 #' @param model a string to be passed (or an object returned from) \code{\link{mirt.model}},
 #'   declaring how the IRT model is to be estimated (loadings, constraints, priors, etc).
 #'   For exploratory IRT models, a single numeric value indicating the number
