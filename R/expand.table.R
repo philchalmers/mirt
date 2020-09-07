8d80154e8cc87c07d6ccd8fcd7de0afc4698b9be
philchalmers <rphilip.chalmers@gmail.com>
2020-09-07 13:17:31 -0400

    generalize expand.table

--- R/expand.table.R
+++ R/expand.table.R
@@ -1,12 +1,19 @@
 #' Expand summary table of patterns and frequencies
 #'
 #' The \code{expand.table} function expands a summary table of unique response
-#' patterns to a full sized data-set. The response frequencies must be on the
-#' rightmost column of the input data.
+#' patterns to a full sized data-set. By default the response frequencies are
+#' assumed to be on rightmost column of the input data, though this can be modified.
 #'
 #' @param tabdata An object of class \code{data.frame} or \code{matrix}
 #'   with the unique response patterns and the number of frequencies
-#'   in the rightmost column
+#'   in the rightmost column (though see \code{freq} for details on how to omit this
+#'   column)
+#' @param freq either a character vector specifying the column in \code{tabdata}
+#'   to be used as the frequency count indicator for each response pattern (defaults to
+#'   the right-most column) or a integer vector of length \code{nrow(tabdata)} specifying
+#'   the frequency counts. When using the latter approach the \code{tabdata} input should not
+#'   include any information regarding the counts, and instead should only include the unique
+#'   response patterns themselves
 #' @param sample logical; randomly switch the rows in the expanded table? This does not change the
 #'   expanded data, only the row locations
 #' @return Returns a numeric matrix with all the response patterns.
@@ -21,18 +28,81 @@
 #' @examples
 #'
 #' data(LSAT7)
-#' head(LSAT7)
+#' head(LSAT7) # frequency in right-most column
 #' LSAT7full <- expand.table(LSAT7)
 #' head(LSAT7full)
+#' dim(LSAT7full)
 #'
-#' LSAT7full <- expand.table(LSAT7, sample = TRUE)
-#' head(LSAT7full)
+#' # randomly switch rows in the expanded response table
+#' LSAT7samp <- expand.table(LSAT7, sample = TRUE)
+#' head(LSAT7samp)
+#' colMeans(LSAT7full)
+#' colMeans(LSAT7samp) #equal
 #'
-expand.table <- function(tabdata, sample = FALSE) {
+#' #--------
+#'
+#' \dontrun{
+#' # Generate data from separate response pattern matrix and freq vector
+#' # The following uses Table 2.1 from de Ayala (2009)
+#' f <- c(691,2280,242,235,158,184,1685,1053,134,462,92,65,571,79,87,41,1682,702,
+#'        370,63,626,412,166,52,28,15,2095,1219,500,187,40,3385)
+#'
+#' longpat <- scan()
+#' 0 0 0 0 0
+#' 1 0 0 0 0
+#' 0 1 0 0 0
+#' 0 0 1 0 0
+#' 0 0 0 1 0
+#' 0 0 0 0 1
+#' 1 1 0 0 0
+#' 1 0 1 0 0
+#' 0 1 1 0 0
+#' 1 0 0 1 0
+#' 0 1 0 1 0
+#' 0 0 1 1 0
+#' 1 0 0 0 1
+#' 0 1 0 0 1
+#' 0 0 1 0 1
+#' 0 0 0 1 1
+#' 1 1 1 0 0
+#' 1 1 0 1 0
+#' 1 0 1 1 0
+#' 0 1 1 1 0
+#' 1 1 0 0 1
+#' 1 0 1 0 1
+#' 1 0 0 1 1
+#' 0 1 1 0 1
+#' 0 1 0 1 1
+#' 0 0 1 1 1
+#' 1 1 1 1 0
+#' 1 1 1 0 1
+#' 1 1 0 1 1
+#' 1 0 1 1 1
+#' 0 1 1 1 1
+#' 1 1 1 1 1
+#'
+#'
+#' pat <- matrix(longpat, byrow=TRUE, ncol=5)
+#' colnames(pat) <- paste0('Item.', 1:5)
+#' head(pat)
+#'
+#' table2.1 <- expand.table(pat, freq = f)
+#' dim(table2.1)
+#'
+#' }
+#'
+#'
+expand.table <- function(tabdata, freq = colnames(tabdata)[ncol(tabdata)],
+                         sample = FALSE) {
     if(missing(tabdata)) missingMsg('tabdata')
     stopifnot(is.data.frame(tabdata) || is.matrix(tabdata))
-    freq <- tabdata[,ncol(tabdata)]
-    tabdata <- tabdata[,-ncol(tabdata), drop=FALSE]
+    if(is.character(freq)){
+        stopifnot(length(freq) == 1L)
+        tmp <- tabdata[,freq]
+        tabdata <- subset(tabdata, select = -freq)
+        freq <- tmp
+    }
+    stopifnot(length(freq) == nrow(tabdata))
     fulldata <- vector('list', nrow(tabdata))
     for (i in seq_len(nrow(tabdata)))
         fulldata[[i]] <- tabdata[rep(i, freq[i]), ]
