6ef8d1a2743b3ced397966c75cdca771de38c0cf
philchalmers <rphilip.chalmers@gmail.com>
2021-02-25 11:24:23 -0500

    classic drop=FALSE strikes again... fixes # 201

--- R/DIF.R
+++ R/DIF.R
@@ -176,7 +176,8 @@ DIF <- function(MGmodel, which.par, scheme = 'add', items2test = 1:extract.mirt(
         constrain <- model@Model$constrain
         mirt_model <- model@Model$model
         if(is(mirt_model, 'mirt.model'))
-            mirt_model$x <- mirt_model$x[mirt_model$x[,"Type"] != 'CONSTRAINB', ]
+            mirt_model$x <- mirt_model$x[mirt_model$x[,"Type"] != 'CONSTRAINB',
+                                         , drop=FALSE]
         technical$omp <- FALSE
         parnum <- list()
         for(i in seq_len(length(which.par)))
