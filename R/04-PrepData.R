e8dcc7d2ae702fd49533b4a7482c791dfae1bf1a
philchalmers <rphilip.chalmers@gmail.com>
2021-07-06 12:11:21 -0400

    earlier check that rating scale models are unidim

--- R/04-PrepData.R
+++ R/04-PrepData.R
@@ -84,6 +84,9 @@ PrepData <- function(data, model, itemtype, guess, upper, gpcm_mats, opts,
     factorNames <- setdiff(model$x[,1L],keywords)
     nfactNames <- length(factorNames)
     nfact <- sum(!grepl('\\(',factorNames))
+    if(nfact > 1L && any(itemtype %in% 'rsm', 'gpcmIRT', 'grsmIRT'))
+        stop(c('Rating scale models based on classical IRT parametrization ',
+             'are not generalizable to multidimensional models'), call. = FALSE)
     index <- 1L:J
     Names <- NULL
     for(i in 1L:J)
