b1e7a6ebc515d4020bdc89cb2328a269c7c9968a
Phil Chalmers <rphilip.chalmers@gmail.com>
2024-06-28 12:46:22 -0400

    increase functionality

--- R/RCI.R
+++ R/RCI.R
@@ -6,7 +6,9 @@
 #' of response pattern information in the pre/post data score estimates, as well
 #' as conditional standard error of measurement information.
 #'
-#' @param mod single-group model fitted by \code{\link{mirt}}
+#' @param mod single-group model fitted by \code{\link{mirt}}. If not supplied the
+#'  information will be extracted from the data input objects to compute the classical
+#'  test theory version of the RCI statistics
 #' @param predat a vector (if one individual) or matrix/data.frame
 #'   of response data to be scored, where each individuals' responses are
 #'   included in exactly one row
@@ -14,6 +16,16 @@
 #'   measurement
 #' @param cutoffs optional vector of length 2 indicating the type of cut-offs to
 #'   report (e.g., \code{c(-1.96, 1.96)} reflects the 95 percent z-score type cut-off)
+#'
+#' @param rxx.pre CTT reliability of pretest. If not supplied will be computed using coefficient
+#'  alpha from \code{predat}
+#' @param rxx.post same as \code{rxx.pre}, but for post-test data
+#' @param SD.pre standard deviation of pretest. If not supplied will be computed from \code{predat}
+#' @param SD.post same as \code{SD.pre}, but for the post-test data
+#' @param rxx.method for CTT version of RCI, which method to use for pooling the reliability
+#'   information. Currently supports \code{'pooled'} to pool the pre-post
+#'   reliability estimates (default) or \code{'pre'} for using just the pre-test
+#'
 #' @param ... additional arguments passed to \code{\link{fscores}}
 #'
 #' @author Phil Chalmers \email{rphilip.chalmers@@gmail.com}
@@ -31,20 +43,49 @@
 #'
 #' \dontrun{
 #'
-#' mod <- mirt(Science, 1)
+#' # simulate some data
+#' N <- 1000
+#' J <- 20     # number of items
+#' a <- matrix(rlnorm(J,.2,.3))
+#' d <- rnorm(J)
+#'
+#' theta <- matrix(rnorm(N))
+#' dat_pre <- simdata(a, d, itemtype = '2PL', Theta = theta)
+#'
+#' # first 3 cases decrease by 1/2
+#' theta2 <- theta - c(1/2, 1/2, 1/2, numeric(N-3))
+#' dat_post <- simdata(a, d, itemtype = '2PL', Theta = theta2)
+#'
+#' mod <- mirt(dat_pre)
+#'
+#' # all changes using fitted model from pre data
+#' RCI(mod, predat=dat_pre, postdat=dat_post)
 #'
 #' # single response pattern change using EAP information
-#' RCI(mod, predat = c(1,2,3,2), postdat = c(1,2,2,1))
+#' RCI(mod, predat=dat_pre[1,], postdat=dat_post[1,])
 #'
 #' # WLE estimator
-#' RCI(mod, predat = c(1,2,3,2), postdat = c(1,2,2,1), method = 'WLE')
+#' RCI(mod, predat = dat_pre[1,], postdat = dat_post[1,], method = 'WLE')
 #'
 #' # multiple respondents
-#' RCI(mod, predat = Science[1:5,], postdat = Science[2:6,])
+#' RCI(mod, predat = dat_pre[1:6,], postdat = dat_post[1:6,])
 #'
 #' # include large-sample z-type cutoffs
-#' RCI(mod, predat = Science[1:5,], postdat = Science[2:6,],
+#' RCI(mod, predat = dat_pre[1:6,], postdat = dat_post[1:6,],
 #'     cutoffs = c(-1.96, 1.96))
+#'
+#' ######
+#' # CTT version by omitting IRT model (easiest to use complete dataset)
+#' RCI(predat = dat_pre, postdat = dat_post)
+#'
+#' # CTT version with pre-computed information
+#' RCI(predat = dat_pre[1:6,], postdat = dat_post[1:6,],
+#'     rxx.pre=.6, rxx.post=.6, SD.pre=2, SD.post=3,
+#'     cutoffs = c(-1.96, 1.96))
+#'
+#' # just pre-test rxx
+#' RCI(predat = dat_pre[1:6,], postdat = dat_post[1:6,],
+#'     rxx.pre=.6, SD.pre=2, rxx.method = 'pre')
 #'
 #' ############################
 #' # Example where individuals take completely different item set pre-post
@@ -66,19 +107,55 @@
 #' RCI(mod, predat, postdat)
 #'
 #' }
-RCI <- function(mod, predat, postdat, cutoffs = NULL, ...){
-    stopifnot(extract.mirt(mod, 'nfact') == 1L)
+RCI <- function(mod, predat, postdat, cutoffs = NULL,
+                rxx.method = 'pooled',
+                rxx.pre = NULL, rxx.post = NULL,
+                SD.pre = NULL, SD.post = NULL, ...){
     if(!is.null(cutoffs))
         stopifnot(length(cutoffs) == 2)
-    fs_pre <- fscores(mod, response.pattern = predat, ...)
-    fs_post <- fscores(mod, response.pattern = postdat, ...)
+    if(missing(mod)){
+        stopifnot(rxx.method %in% c('pooled', 'pre'))
+        if(is.vector(predat))
+            predat <- matrix(predat, 1L)
+        if(is.vector(postdat))
+            postdat <- matrix(postdat, 1L)
+        TS_pre <- rowSums(predat)
+        TS_post <- rowSums(postdat)
+        if(is.null(SD.pre)) SD.pre <- sd(TS_pre)
+        if(is.null(SD.post)) SD.post <- sd(TS_post)
+        if(is.null(rxx.pre))
+            rxx.pre <- itemstats(predat)$overall$alpha
+        if(rxx.method == 'pre')
+            rxx.post <- rxx.pre
+        if(is.null(rxx.post) && rxx.method == 'pooled')
+            rxx.post <- itemstats(postdat)$overall$alpha
+        SEM_pre <- as.numeric(SD.pre * sqrt(1 - rxx.pre))
+        SEM_post <- as.numeric(SD.post * sqrt(1 - rxx.post))
+        if(rxx.method == 'pooled'){
+            SEM <- sqrt(SEM_pre^2 + SEM_post^2)
+        } else if(rxx.method == 'pre'){
+            SEM <- sqrt(2*SEM_pre^2)
+        }
+        diff <- TS_post - TS_pre
+        z_JCI <- diff / SEM
+        ret <- data.frame(pre.score=TS_pre, post.score=TS_post, diff,
+                          SEM=SEM, z=z_JCI)
+    } else {
+        if(extract.mirt(mod, 'nfact') == 1L){
+            fs_pre <- fscores(mod, response.pattern = predat, ...)
+            fs_post <- fscores(mod, response.pattern = postdat, ...)
 
-    diff <- fs_post[,1] - fs_pre[,1]
-    pse <- sqrt(fs_pre[,2]^2 + fs_post[,2]^2)
-    ret <- data.frame(pre.score=fs_pre[,1], post.score=fs_post[,1], diff,
-                      pooled_SEM=pse, z=diff/pse)
+            diff <- fs_post[,1] - fs_pre[,1]
+            pse <- sqrt(fs_pre[,2]^2 + fs_post[,2]^2)
+            ret <- data.frame(pre.score=fs_pre[,1], post.score=fs_post[,1], diff,
+                              pooled_SEM=pse, z=diff/pse)
+        } else {
+            stop('not yet supported')
+            browser()
+        }
+    }
     rownames(ret) <- NULL
-    if(!is.null(cutoffs)){
+    if(!is.null(cutoffs) && ncol(ret) == 5L){ # only for unidim
         ret$cut_decision <- 'unchanged'
         ret$cut_decision[ret$z > max(cutoffs)] <- 'increased'
         ret$cut_decision[ret$z < min(cutoffs)] <- 'decreased'
