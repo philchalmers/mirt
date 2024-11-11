0581f8a6de18626e86c0217ba79bd1f5f6f3f4da
kss2k <slupphaugkjell@gmail.com>
2024-11-11 14:04:38 +0100

    fix groupName partial match

--- R/utils.R
+++ R/utils.R
@@ -2428,7 +2428,7 @@ MGC2SC <- function(x, which){
     }
     tmp@Data <- x@Data
     tmp@Data$completely_missing <- integer(0L)
-    tmp@Data$data <- tmp@Data$data[tmp@Data$group == tmp@Data$groupName[which], , drop=FALSE]
+    tmp@Data$data <- tmp@Data$data[tmp@Data$group == tmp@Data$groupNames[which], , drop=FALSE]
     tmp@Data$rowID <- 1L:nrow(tmp@Data$data)
     tmp@Data$Freq[[1L]] <- tmp@Data$Freq[[which]]
     tmp@Data$fulldata[[1L]] <- x@Data$fulldata[[which]]
