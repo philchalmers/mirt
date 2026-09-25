8cac4689fafc8d3bf9e10797ea124fa50129e811
philchalmers <rphilip.chalmers@gmail.com>
2026-09-25 10:57:30 -0400

    add freq

--- R/empirical_plot.R
+++ R/empirical_plot.R
@@ -20,15 +20,22 @@
 #' @param which.items a numeric vector indicating which items to plot in a faceted image plot.
 #'   If NULL then empirical test plots will be constructed instead
 #' @param smooth logical; include a GAM smoother instead of the raw proportions? Default is FALSE
-#' @param type character vector specifying type of plot to draw. When \code{which.item} is NULL
-#'   can be 'prop' (default) for cumulative proportions,
-#'   'hist' for histogram of total scores, and
-#'   'discrim' for plotting reduced item-total correlations. When \code{which.item} is not NULL can
-#'   be 'prop' (default) for item-level conditional proportions against reduced total scores,
-#'   'boxplot' for conditional boxplots of reduced total scores, and 'bubble' for bivariate frequency
-#'   buble plots.
+#' @param type character vector specifying type of plot to draw, some of which change as a function of
+#'   the \code{which.item} input.
 #'
-#'   When \code{type} is 'bubble' \code{which.items} must have length two
+#'   \describe{
+#'    \item{'prop' (default)}{cumulative total-score proportions. If  \code{which.items}
+#'      specified then item-level conditional proportions are instead plotted
+#'      against the reduced total scores}
+#'    \item{'hist'}{histogram of total scores}
+#'    \item{'discrim'}{reduced item-total correlations to visualize discrimination effects}
+#'    \item{'difficulty'}{mean/proportion of each item}
+#'    \item{'discrim_diff'}{reduced item-total correlation against item difficulty}
+#'    \item{'boxplot'}{conditional boxplots of reduced total scores (supports \code{which.item})}
+#'    \item{'bubble'}{bivariate frequency  bubble plots (requires that \code{which.items}
+#'      is exactly of length two)}
+#'   }
+#'
 #' @param sort logical; when applicable, sort the items first (e.g., in discrimination plot)?
 #' @param formula formula used for the GAM smoother
 #' @param main the main title for the plot. If NULL an internal default will be used
@@ -57,8 +64,15 @@
 #' empirical_plot(data)
 #' empirical_plot(data, type = 'hist')
 #' empirical_plot(data, type = 'hist', breaks=20)
+#' empirical_plot(data, type = 'discrim')
+#' empirical_plot(data, type = 'discrim', sort=TRUE)
+#' empirical_plot(data, type = 'difficulty')
+#' empirical_plot(data, type = 'difficulty', sort=TRUE)
+#' empirical_plot(data, type = 'discrim_diff')
+#' empirical_plot(data, type = 'freq')
 #'
 #' # items 1, 2 and 5
+#' empirical_plot(data, c(1, 2, 5), type = 'freq')
 #' empirical_plot(data, c(1, 2, 5))
 #' empirical_plot(data, c(1, 2, 5), smooth = TRUE)
 #' empirical_plot(data, c(1, 2, 5), type = 'boxplot')
@@ -80,12 +94,17 @@
 #'
 #'
 #' #################
-#' # polytomous
+#' # polytomous response data
 #' empirical_plot(Science)
 #' empirical_plot(Science, type = 'hist', breaks=20)
+#' empirical_plot(Science, type = 'freq')
+#' empirical_plot(Science, type = 'difficulty')
+#' empirical_plot(Science, type = 'discrim_diff')
+#' empirical_plot(Science, type = 'boxplot')
+#'
+#' # item-level
 #' empirical_plot(Science, c(1, 2), type = 'bubble')
 #' empirical_plot(Science, c(1, 3), type = 'bubble')
-#' empirical_plot(Science, which.items = 1:4, type = 'boxplot')
 #' empirical_plot(Science, which.items = 1:4, type = 'prop')
 #' empirical_plot(Science, which.items = 1:4, type = 'prop', smooth=TRUE)
 #'
@@ -94,23 +113,35 @@
 #' mod <- mirt(Science)
 #' plot(mod, type='trace')
 #'
+#' # when missing values present
+#' Science[1:3, 1] <- NA
+#' Science[6:8, 2] <- NA
+#' empirical_plot(Science, type = 'freq')
+#'
+#'
 #' }
 empirical_plot <- function(data, which.items = NULL, type = 'prop',
-                           smooth = FALSE, sort=TRUE, formula = resp ~ s(TS, k = 5),
+                           smooth = FALSE, sort=FALSE, formula = resp ~ s(TS, k = 5),
                            org.data = NULL,
                            discrim.cut = .2, main = NULL, par.strip.text = list(cex = 0.7),
                            par.settings = list(strip.background = list(col = '#9ECAE1'),
                                                strip.border = list(col = "black")),
                            auto.key = list(space = 'right', points=FALSE, lines=TRUE), ...){
     stopifnot(is.matrix(data) || is.data.frame(data))
-    stopifnot(type %in% c('prop', 'hist', 'boxplot', 'bubble', 'discrim'))
+    if(is.null(which.items) && type %in% c("freq", 'boxplot'))
+        which.items <- 1:ncol(data)
+    stopifnot(type %in% c('prop', 'hist', 'boxplot', 'bubble',
+                          'discrim', 'difficulty', 'discrim_diff', 'freq'))
     if(is.null(which.items))
-        stopifnot("Must specify which.items"=type %in% c('prop', 'hist', 'discrim'))
+        stopifnot("Must specify which.items"=type %in%
+                      c('prop', 'hist', 'discrim', 'difficulty', 'discrim_diff'))
     if(type %in% c('boxplot', 'bubble')) smooth <- FALSE
-    data <- na.omit(as.matrix(data))
+    if(!(type %in% c('freq')))
+        data <- na.omit(as.matrix(data))
     if(is.null(org.data))
         org.data <- data
-    org.data <- na.omit(as.matrix(org.data))
+    if(!(type %in% c('freq')))
+        org.data <- na.omit(as.matrix(org.data))
     stopifnot("dimensions of org.data do not match data" =
                   all(dim(data) == dim(org.data)))
     K <- apply(org.data, 2, function(x) length(unique(x)))
@@ -129,21 +160,64 @@ empirical_plot <- function(data, which.items = NULL, type = 'prop',
     org.data <- org.data[ord,]
     TS <- TS[ord]
     tab <- table(TS)
-    if(type == 'discrim'){
-        is <- itemstats(data)$itemstats
+    if(type %in% c('discrim', 'difficulty', 'discrim_diff', 'freq')){
+        isummary <- itemstats(data)
+        is <- isummary$itemstats
         is$item <- factor(rownames(is), levels=colnames(data))
-        if(sort)
-            is <- is[order(is$total.r_if_rm),]
-        plt <- lattice::xyplot(total.r_if_rm ~ item, is,
-                               pch = 16,
-                               panel = function(x, y, ...) {
-                                   panel.xyplot(x, y, ...)
-                                   panel.abline(h = discrim.cut, col='red', lty=2)
-                               },
-                               main = if(is.null(main)) "Reduced total score correlation" else main,
-                               xlab = 'Item', ylab='Correlation',
-                               scales = list(x = list(rot = 90)))
-        return(plt)
+        if(type == 'discrim'){
+            if(sort){
+                is <- is[order(is$total.r_if_rm),]
+                is$item <- factor(as.character(is$item), levels=as.character(is$item))
+            }
+            plt <- lattice::xyplot(total.r_if_rm ~ item, is,
+                                   pch = 16,
+                                   panel = function(x, y, ...) {
+                                       panel.xyplot(x, y, ...)
+                                       panel.abline(h = discrim.cut, col='red', lty=2)
+                                   },
+                                   main = if(is.null(main)) "Reduced item-total correlation" else main,
+                                   xlab = 'Item', ylab='Correlation',
+                                   scales = list(x = list(rot = 90)))
+            return(plt)
+        }
+        if(type == 'difficulty'){
+            if(sort){
+                is <- is[order(is$mean),]
+                is$item <- factor(as.character(is$item), levels=as.character(is$item))
+            }
+            plt <- lattice::xyplot(mean ~ item, is,
+                                   pch = 16,
+                                   main = if(is.null(main)) "Item Difficulty" else main,
+                                   xlab = 'Item', ylab='Mean',
+                                   scales = list(x = list(rot = 90)))
+            return(plt)
+        }
+        if(type == 'discrim_diff'){
+            plt <- lattice::xyplot(total.r_if_rm ~ mean, is,
+                                   pch = 16,
+                                   panel = function(x, y, subscripts, ...) {
+                                       panel.xyplot(x, y, ...)
+                                       panel.text(x, y, labels = is$item[subscripts],
+                                                  pos = 3, offset = 0.8, cex = 0.75, col = "black")
+                                   },
+                                   main = if(is.null(main)) "Difficulty by Discrimination" else main,
+                                   xlab = 'Mean', ylab='Reduced item-total correlation')
+            return(plt)
+        }
+        if(type == 'freq'){
+            Freq <- cbind(item=is$item, isummary$proportions * isummary$overall$N)[which.items,]
+            mlt <- reshape(Freq, varying=list(which(colnames(Freq) != 'item')),
+                           direction='long', v.names='freq', timevar = 'cat',
+                           times=colnames(isummary$proportions))
+            mlt$cat[is.na(mlt$cat)] <- '<NA>'
+            mlt$cat <- factor(mlt$cat)
+            col <- rep('#9ECAE1', length(levels(mlt$cat)))
+            if(any(mlt$cat == '<NA>')) col[length(col)] <- 'red'
+            plt <- lattice::barchart(freq ~ cat|item, mlt, horizontal = FALSE,
+                                     ylab = 'Frequency', xlab = 'Category', col = col,
+                              main = if(is.null(main)) "Item Response Frequency" else main)
+            return(plt)
+        }
     }
     if(type == 'bubble'){
         stopifnot("which.items must be of length two"=length(which.items) == 2)
